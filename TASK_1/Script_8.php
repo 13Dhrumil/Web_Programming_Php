9972c145ffd2fb76bd91ab6da18d6ba956f6dcbd
Dhabaliya Dhrumil <150112760+13Dhrumil@users.noreply.github.com>
2024-08-26 13:17:35 +0530

    Add files via upload

---
+++ TASK_1/Script_8.php
@@ -0,0 +1,11 @@
+<?php
+
+# Declare variables for a person's name, age, and city.
+
+$name = "Dhabaliya Dhrumil Kiritbhai" ;
+$age = 21;
+$city = "Rajkot";
+
+echo "My name is $name, I am $age years old and I am stay in $city.";
+
+?>
