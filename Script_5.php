230458c41aae8a48037970a9823625ded1160bd7
Dhabaliya Dhrumil <150112760+13Dhrumil@users.noreply.github.com>
2024-08-26 13:05:00 +0530

    Add files via upload

---
+++ Script_5.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Declare two variables, $firstName and $lastName, and assign your first and last name to
+them and print */
+
+$firstName = "Dhrumil ";
+$lastName = "Dhabaliya";
+
+echo $firstName . $lastName;
+
+?>
