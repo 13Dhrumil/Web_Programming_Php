38e442bc65e6b32f745933541cd216af2325658a
Dhabaliya Dhrumil <150112760+13Dhrumil@users.noreply.github.com>
2024-08-26 13:20:18 +0530

    Delete Script_9.php

--- Script_9.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/* Use the echo statement to display a message on multiple lines including the person's
-information. */
-
-echo "<h1>MY SELF</h1> <br> <b>Name :</b> Dhabaliya Dhrumil Kiritbhai<br> <b>Date of Birth :</b> 13/06/2002 <br> <b>Age :</b> 23 <br> <b>Gender :</b> male <br> <b>Cast :</b> Kadva Patel <br> <b>Religion :</b> Hindu <br> <b>Address :</b> 'Meet' Gulab Vihar main road opp street no. 6, behind big bazzar, Rajkot, Gujarat - 360005 <br> <b>Mobile No. :</b> +91 6355752352";
-
-?>
