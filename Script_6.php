d8fe993b668efd11e93fd88de02c05318371c9aa
Dhabaliya Dhrumil <150112760+13Dhrumil@users.noreply.github.com>
2024-08-26 13:20:10 +0530

    Delete Script_6.php

--- Script_6.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-# Create a PHP script that includes both HTML and PHP include bold ,italic and heading tag
-
-echo "<i><h1>WELCOME TO ATMIYA UNIVERSITY</h1></i><br><b>ATMIYA UNIVERSITY is hartly welcome to all.</b>";
-
-?>
